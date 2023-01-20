9b2a1a0fbdfc041d5118c14f2dfa5f0a6c987bd9
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-01-20 15:27:20 +0000

    Added next button to go to the next question

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,17 +2,25 @@ let timerEl = document.querySelector("#time");
 let startButton = document.querySelector("#start");
 let questionTitle = document.querySelector("#question-title");
 let choices = document.querySelector("#choices");
-document.querySelector("#questions").classList.add("hide");
+let questionSection = document.querySelector("#questions");
+let endOfQuizMessage = document.querySelector("#end-screen");
+
+let nextButton = document.createElement("button");
+nextButton.textContent = "Next";
+questionSection.appendChild(nextButton); 
 
 let secondsLeft = 75;
 let score = 0;
 let clicked = false;
 
+function renderQuestionTitle(currentQuestion) {
+  questionTitle.innerHTML = "";
+  questionTitle.textContent = questions[currentQuestion][0];
+}
 
 function renderAnswers(currentQuestion) {
-  document.querySelector("#questions").classList.remove("hide");
+  choices.innerHTML = "";
   for (let j = 0; j < questions[currentQuestion][1].length; j++) {
-    
     let answerEl = document.createElement("button");
     answerEl.setAttribute("data-index", j);
     answerEl.textContent = questions[currentQuestion][1][j];
@@ -22,11 +30,14 @@ function renderAnswers(currentQuestion) {
       event.preventDefault();
       let message = document.createElement("h4");
       choices.appendChild(message);
-      
-      if (parseInt(answerEl.getAttribute("data-index")) === questions[currentQuestion][2]) {
+
+      if (
+        parseInt(answerEl.getAttribute("data-index")) ===
+        questions[currentQuestion][2]
+      ) {
         message.textContent = "Correct!";
         score++;
-        
+        console.log(score);
       } else {
         message.textContent = "Wrong!";
         secondsLeft -= 10;
@@ -35,41 +46,38 @@ function renderAnswers(currentQuestion) {
   }
 }
 
-
-
 function renderQuestion(currentQuestion) {
+  document.querySelector("#end-screen").classList.add("hide");
   document.querySelector("#questions").classList.remove("hide");
-  questionTitle.textContent = questions[currentQuestion][0];
-
+  renderQuestionTitle(currentQuestion);
   renderAnswers(currentQuestion);
-  clicked = true;
+  nextButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (currentQuestion + 1 < questions.length) {
+      renderQuestion(currentQuestion + 1);
+    }
+    else {
+      document.querySelector("#questions").classList.add("hide");
+      document.querySelector("#end-screen").classList.remove("hide");
+    }
+  });
 }
-
 
 startButton.addEventListener("click", function (event) {
   event.preventDefault();
-  for (let i = 0; i < questions.length; i++) {
-    questionTitle.classList.add("hide");
-    choices.classList.add("hide");
-
-    //renderQuestion(i);
-  }
-   // Sets interval in variable
-   var timerInterval = setInterval(function () {
+  renderQuestion(0);
+  // Sets interval in variable
+  var timerInterval = setInterval(function () {
     secondsLeft--;
     timerEl.textContent = secondsLeft;
     if (secondsLeft === 0) {
       clearInterval(timerInterval);
     }
   }, 1000);
-
 });
 
+//renderQuestion(0);
 
-// renderQuestion(0);
-// if (clicked) {
-//    renderQuestion(1);
-// }
 
-renderQuestion(0);
-//renderAnswers(1);
+
+
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,22 +1,17 @@
 let questions = [
   [
     'How do you write "Hello World" in an alert box?',
-      [
-        'alert("Hello World")',
+    [
+      'alert("Hello World")',
       'alertBox("Hello World")',
       'msg("Hello World")',
-      'msgBox("Hello World")'
+      'msgBox("Hello World")',
     ],
-      0
+    0,
   ],
   [
     "How to write an IF statement in JavaScript?",
-     [
-      "if i = 5",
-      "if (i = 5) then",
-      "if (i === 5)",
-      "if i == 5 then"
-    ],
-      2
-    ],
+    ["if i = 5", "if (i = 5) then", "if (i === 5)", "if i == 5 then"],
+    2,
+  ],
 ];
