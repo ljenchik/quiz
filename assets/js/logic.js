9816f06f14cd1714ff8a4ddf3c285b83b3f19141
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-01-20 13:48:22 +0000

    Added renderAnswers() function to display the answers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,48 +2,74 @@ let timerEl = document.querySelector("#time");
 let startButton = document.querySelector("#start");
 let questionTitle = document.querySelector("#question-title");
 let choices = document.querySelector("#choices");
+document.querySelector("#questions").classList.add("hide");
 
 let secondsLeft = 75;
-let numberOfQuestions = questions.length;
-let index = 0;
+let score = 0;
+let clicked = false;
 
-function renderQuestion() {
+
+function renderAnswers(currentQuestion) {
   document.querySelector("#questions").classList.remove("hide");
-  questionTitle.textContent = questions[index][0];
-
-  for (let j = 0; j < questions[index][1].length; j++) {
+  for (let j = 0; j < questions[currentQuestion][1].length; j++) {
+    
     let answerEl = document.createElement("button");
     answerEl.setAttribute("data-index", j);
-    answerEl.textContent = questions[index][1][j];
+    answerEl.textContent = questions[currentQuestion][1][j];
     choices.appendChild(answerEl);
 
     answerEl.addEventListener("click", function (event) {
       event.preventDefault();
-
       let message = document.createElement("h4");
       choices.appendChild(message);
-
-      console.log(answerEl.getAttribute("data-index"));
-
-      if (parseInt(answerEl.getAttribute("data-index")) === questions[0][2]) {
+      
+      if (parseInt(answerEl.getAttribute("data-index")) === questions[currentQuestion][2]) {
         message.textContent = "Correct!";
+        score++;
+        
       } else {
         message.textContent = "Wrong!";
+        secondsLeft -= 10;
       }
     });
   }
 }
 
+
+
+function renderQuestion(currentQuestion) {
+  document.querySelector("#questions").classList.remove("hide");
+  questionTitle.textContent = questions[currentQuestion][0];
+
+  renderAnswers(currentQuestion);
+  clicked = true;
+}
+
+
 startButton.addEventListener("click", function (event) {
   event.preventDefault();
-  renderQuestion();
-  // Sets interval in variable
-  var timerInterval = setInterval(function () {
+  for (let i = 0; i < questions.length; i++) {
+    questionTitle.classList.add("hide");
+    choices.classList.add("hide");
+
+    //renderQuestion(i);
+  }
+   // Sets interval in variable
+   var timerInterval = setInterval(function () {
     secondsLeft--;
     timerEl.textContent = secondsLeft;
     if (secondsLeft === 0) {
       clearInterval(timerInterval);
     }
   }, 1000);
+
 });
 
+
+// renderQuestion(0);
+// if (clicked) {
+//    renderQuestion(1);
+// }
+
+renderQuestion(0);
+//renderAnswers(1);
